ae8151228438cd5f62bcbfbc1c1eba396392915d
Anjana Wijesooriya <ajgihan@gmail.com>
2019-07-12 10:00:07 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -13,10 +13,10 @@ const port = 5000;
 // the mysql.createConnection function takes in a configuration object which contains host, user, password and the database name.
 //mysql://ba9565c7d6951a:5230e113@us-cdbr-iron-east-02.cleardb.net/heroku_f254459ae2fc71b?reconnect=true 
 const db = mysql.createConnection ({
-    host: 'q3vtafztappqbpzn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-    user: 's8bk87fesahj71v0',
-    password: 'xevvy56o7qebr1k0',
-    database: 'h6m2g8nzpq6q8cxr', // FYI export the tshirtshop.sql to this database
+    host: 'us-cdbr-iron-east-02.cleardb.net',
+    user: 'b22789322cfb92',
+    password: 'b7499b6f',
+    database: 'heroku_fb4bfcaea035c93', // FYI export the tshirtshop.sql to this database
     multipleStatements: true
 });
 
